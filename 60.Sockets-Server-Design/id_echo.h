ed03ffb82edb6349f3186e4451495b6b9e52da06
sagsango <sagar.it.iiests@gmail.com>
2022-02-22 05:55:09 +0530

    ++Sockets-Server-Design

---
+++ 60.Sockets-Server-Design/id_echo.h
@@ -0,0 +1,11 @@
+/* id_echo.h
+
+   Header file for id_echo_sv.c and id_echo_cl.c.
+*/
+#include "inet_sockets.h"       /* Declares our socket functions */
+#include "tlpi_hdr.h"
+
+#define SERVICE "echo"          /* Name of UDP service */
+
+#define BUF_SIZE 500            /* Maximum size of datagrams that can
+                                   be read by client and server */
